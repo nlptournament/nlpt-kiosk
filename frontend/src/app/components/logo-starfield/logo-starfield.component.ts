e2ec1676b520bd24b38dfd6a8f9d5bb475697a16
Nils Ost <home@nijos.de>
2025-02-04 22:23:19 +0100

    frontend starfield logo screen

---
+++ frontend/src/app/components/logo-starfield/logo-starfield.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-starfield',
+  imports: [],
+  templateUrl: './logo-starfield.component.html',
+  styleUrl: './logo-starfield.component.scss'
+})
+export class LogoStarfieldComponent {
+    title: string | undefined | null;
+    subtitle: string | undefined | null;
+}
