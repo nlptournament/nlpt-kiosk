9015f593af9b1484545cba41eeaf15d5fa466188
Nils Ost <home@nijos.de>
2025-06-24 09:06:06 +0200

    frontend: Sceens can now be hidden in ScreensPanel; added filter to ScreensPanels

--- frontend/src/app/components/admin/screens-panel/screens-panel.component.ts
+++ frontend/src/app/components/admin/screens-panel/screens-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, model } from '@angular/core';
+import { Component, input, model, OnInit } from '@angular/core';
 
 import { ScreenTemplate } from '../../../interfaces/screen-template';
 import { User } from '../../../interfaces/user';
@@ -8,14 +8,28 @@ import { Media } from '../../../interfaces/media';
 import { CommonModule } from '@angular/common';
 import { ScreenComponent } from '../../elements/screen/screen.component';
 import { Dialog } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { TooltipModule } from 'primeng/tooltip';
+import { InputTextModule } from 'primeng/inputtext';
+
+interface selectableUser {
+    code: string | null;
+    name: string;
+}
+
+interface selectableTemplate {
+    code: string | null;
+    name: string;
+}
 
 @Component({
   selector: 'panel-screens',
-  imports: [CommonModule, ScreenComponent, Dialog],
+  imports: [CommonModule, ScreenComponent, Dialog, TooltipModule, FormsModule, SelectModule, InputTextModule],
   templateUrl: './screens-panel.component.html',
   styleUrl: './screens-panel.component.scss'
 })
-export class ScreensPanelComponent {
+export class ScreensPanelComponent implements OnInit {
     screens = input.required<Map<string, Screen>>();
     screenTemplates = input.required<Map<string, ScreenTemplate>>();
     users = input.required<Map<string, User>>();
@@ -24,8 +38,40 @@ export class ScreensPanelComponent {
     isActive = model.required<boolean>();
 
     isVisible: boolean = true;
+    selectableUsers: selectableUser[] = [];
+    selectableTemplates: selectableTemplate[] = [];
     createScreenActive: boolean = false;
     createScreenDummy: Screen = <Screen>{};
+
+    showHiddenScreens: boolean = false;
+    showLockedScreens: boolean = false;
+    ignoreScreenLocks: boolean = true;
+    filterDesc: string = "";
+    filterUser: string | null = null;
+    filterTemplate: string | null = null;
+
+    ngOnInit(): void {
+        this.createSelectableUsers();
+        this.createSelectableTemplates();
+    }
+
+    createSelectableUsers() {
+        let su: selectableUser[] = [];
+        su.push(<selectableUser>{code: null, name: 'user'});
+        for (let k of this.users().keys()) {
+            su.push(<selectableUser>{code: k, name: this.users().get(k)!.login});
+        }
+        this.selectableUsers = su;
+    }
+
+    createSelectableTemplates() {
+        let st: selectableTemplate[] = [];
+        st.push(<selectableTemplate>{code: null, name: 'template'});
+        for (let k of this.screenTemplates().keys()) {
+            st.push(<selectableTemplate>{code: k, name: this.screenTemplates().get(k)!.name});
+        }
+        this.selectableTemplates = st;
+    }
 
     createScreen() {
         this.createScreenDummy = <Screen>{desc: '', user_id: this.currentUser().id, duration: null, repeat: 0, loop: false, variables: {}};
