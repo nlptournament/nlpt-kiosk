3564f202603707251a525e07c5cc85c3f63804ab
Nils Ost <home@nijos.de>
2026-02-16 23:33:25 +0100

    discord fetcher: tested all required functions to build integration

--- backend/helpers/discord.py
+++ backend/helpers/discord.py
@@ -9,43 +9,58 @@ def _discord_process():
     intents.message_content = True
     intents.guilds = True
     intents.members = True
+    intents.presences = True
 
     client = discord.Client(intents=intents)
 
     def capture_game(player):
         if not player.bot and player.guild.id == 544405246810783744:  # TODO: insert guild from settings
-            # TODO: optional check for member.role
+            # if role_filter is active, only scan players having this role
+            role_filter = None  # TODO: insert role_filter from settings
+            if role_filter is not None:
+                for role in player.roles:
+                    if role.id == role_filter:
+                        break
+                else:
+                    return
+
             playing = dict()
             for act in player.activities:
-                if act.type == 'playing':
+                if act.type.name == 'playing':
                     playing[act.timestamps.get('start', 0)] = act.name
             if len(playing) > 0:
                 current_game = playing[sorted(playing.keys(), reverse=True)[0]]
-                print(f'Player {player.id} is playing {current_game}')  # TODO: save to db
+                print(f'Player {player.id} ({player.name}) is playing {current_game}')  # TODO: save to db
             else:
-                print(f'Player {player.id} is playing nothing')  # TODO: save to db
+                print(f'Player {player.id} ({player.name}) is playing nothing')  # TODO: save to db
 
     @client.event
     async def on_ready():
         print(f'We have logged in as {client.user}')
         print('available guilds:')
-        for g in client.guilds:
+        for g in client.guilds:  # TODO: save guilds somewhere the frontend can read them
             print(f'  {g.name}: {g.id}')
         # TODO: initial fetch of all member activities (including dump of DB data???)
         for g in client.guilds:
             if g.id == 544405246810783744:  # TODO: insert guild from settings
-                print('found guild')
+                print('\naavailable roles:')
+                for r in g.roles:  # TODO: save roles somewhere the frontend can read them
+                    print(f'  {r.name}: {r.id}')
                 for m in g.members:
-                    print(f'capture {m.name}')
                     capture_game(m)
+        print('\n')
 
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
 
-        if message.content.startswith('$hello'):
+        """ TODO: make this happen only on DMs
+        if message.content.startswith('/hello'):
             await message.channel.send('Hello!')
+        else:
+            await message.channel.send("Hi! I'm a bot collecting activities about played games, for displaying them on our Kiosk-projectors.")
+        """
 
     # on_presence_update is called when member status or member activity changes
     @client.event
