b6b785b1669c4057e65b2323b8d40612192508b0
Nils Ost <home@nijos.de>
2026-02-23 23:47:02 +0100

    Stream creation Wizard functional

--- frontend/src/app/components/admin/stream-wizard/stream-wizard.component.ts
+++ frontend/src/app/components/admin/stream-wizard/stream-wizard.component.ts
@@ -1,16 +1,21 @@
 import { Component, input, model, OnInit } from '@angular/core';
 
 import { User } from '../../../interfaces/user';
-import { Media, MediaType } from '../../../interfaces/media';
+import { Media, MediaType, MediaSrcType } from '../../../interfaces/media';
 import { Screen } from '../../../interfaces/screen';
 import { ScreenTemplate } from '../../../interfaces/screen-template';
 import { TimelineTemplate } from '../../../interfaces/timeline-template';
+
+import { ScreenService } from '../../../services/screen.service';
+import { MediaService } from '../../../services/media.service';
+import { TimelineTemplateService } from '../../../services/timeline-template.service';
 
 import { CommonModule } from '@angular/common';
 import { Dialog } from 'primeng/dialog';
 import { FormsModule } from '@angular/forms';
 import { SelectModule } from 'primeng/select';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 
 interface selectableElement {
@@ -20,7 +25,7 @@ interface selectableElement {
 
 @Component({
   selector: 'stream-wizard',
-  imports: [CommonModule, Dialog, FormsModule, SelectModule, InputTextModule],
+  imports: [CommonModule, Dialog, FormsModule, SelectModule, InputTextModule, ButtonModule],
   templateUrl: './stream-wizard.component.html',
   styleUrl: './stream-wizard.component.scss'
 })
@@ -39,12 +44,19 @@ export class StreamWizardComponent implements OnInit {
     selectableScreens: selectableElement[] = [];
     selectableTimelineTemplates: selectableElement[] = [];
 
+    streamScreenTemplate: string | null = null;
     selectedUser: string | null = null;
     selectedMedia: string | null = null;
     selectedScreen: string | null = null;
     selectedTimelineTemplate: string | null = null;
     streamUrl: string = "";
     commonDesc: string = "";
+
+    constructor(
+        private screenService: ScreenService,
+        private mediaService: MediaService,
+        private timelineTemplateService: TimelineTemplateService
+    ) {}
 
     ngOnInit(): void {
         this.createSelectableUsers();
@@ -86,19 +98,20 @@ export class StreamWizardComponent implements OnInit {
 
     createSelectableScreens() {
         let ss: selectableElement[] = [];
-        let st_id: string | null = null;
-        for (let k of this.screenTemplates().keys()) {
-            let st: ScreenTemplate = this.screenTemplates().get(k)!;
-            if (st.name == 'Stream') {
-                st_id = st.id;
-                break;
+        if (!this.streamScreenTemplate) {
+            for (let k of this.screenTemplates().keys()) {
+                let st: ScreenTemplate = this.screenTemplates().get(k)!;
+                if (st.name == 'Stream') {
+                    this.streamScreenTemplate = st.id;
+                    break;
+                }
             }
         }
-        if (st_id) {
+        if (this.streamScreenTemplate) {
             ss.push(<selectableElement>{code: null, name: '--create new--'});
             for (let k of this.screens().keys()) {
                 let s: Screen = this.screens().get(k)!;
-                if (s.template_id == st_id && s.user_id == this.selectedUser)
+                if (s.template_id == this.streamScreenTemplate && s.user_id == this.selectedUser)
                     ss.push(<selectableElement>{code: k, name: s.desc});
             }
         }
@@ -118,5 +131,78 @@ export class StreamWizardComponent implements OnInit {
 
     closeDialog() {
         this.isActive.set(false);
+    }
+
+    saveMedia() {
+        if (!this.streamScreenTemplate) return;
+
+        let media: Media;
+        if (this.selectedMedia) {
+            media = this.medias().get(this.selectedMedia)!;
+            media.common = false;
+            this.mediaService.updateMedia(media).subscribe({
+                next: (result: any) => {
+                    this.saveScreen(result['updated']);
+                },
+                error: () => {}
+            });
+        }
+        else {
+            media = <Media>{desc: this.commonDesc, type: MediaType.stream, src_type: MediaSrcType['generic web URL'], src: this.streamUrl, common: false, user_id: this.selectedUser};
+            this.mediaService.createMedia(media).subscribe({
+                next: (result: any) => {
+                    this.saveScreen(result['created']);
+                },
+                error: () => {}
+            });
+        }
+    }
+
+    saveScreen(media_id: string) {
+        if (!this.streamScreenTemplate) return;
+        let screenTemplate: ScreenTemplate = this.screenTemplates().get(this.streamScreenTemplate)!;
+        let screen: Screen;
+        if (this.selectedScreen) {
+            screen = this.screens().get(this.selectedScreen)!;
+            screen.variables = {stream: media_id};
+            this.screenService.updateScreen(screen).subscribe({
+                next: (result: any) => {
+                    this.saveTimelineTemplate(result["updated"]);
+                },
+                error: () => {}
+            });
+        }
+        else {
+            screen = <Screen>{desc: this.commonDesc, key: screenTemplate.key, template_id: screenTemplate.id, user_id: this.selectedUser, variables: {stream: media_id}};
+            this.screenService.createScreen(screen).subscribe({
+                next: (result: any) => {
+                    this.saveTimelineTemplate(result["created"]);
+                },
+                error: () => {}
+            });
+        }
+    }
+
+    saveTimelineTemplate(screen_id: string) {
+        let template: TimelineTemplate;
+        if (this.selectedTimelineTemplate) {
+            template = this.timelineTemplates().get(this.selectedTimelineTemplate)!;
+            template.screen_ids = [screen_id];
+            this.timelineTemplateService.updateTimelineTemplate(template).subscribe({
+                next: (result: any) => {
+                    this.closeDialog();
+                },
+                error: () => {}
+            });
+        }
+        else {
+            template = <TimelineTemplate>{desc: this.commonDesc, user_id: this.selectedUser, screen_ids: [screen_id]};
+            this.timelineTemplateService.createTimelineTemplate(template).subscribe({
+                next: (result: any) => {
+                    this.closeDialog();
+                },
+                error: () => {}
+            });
+        }
     }
 }
